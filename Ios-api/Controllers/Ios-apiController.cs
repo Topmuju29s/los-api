c32cae09c5072ce5eba1124fe662a70fe723e877
Topmuju <worawut.tm@gmail.com>
2021-03-23 21:27:01 +0700

    Init app

---
+++ Ios-api/Controllers/Ios-apiController.cs
@@ -0,0 +1,86 @@
+﻿using Ios_api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ios_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ios_apiController : ControllerBase
+    {
+        private readonly losContext _context;
+        public Ios_apiController(losContext context)
+        {
+            _context = context;
+        }
+
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var Stock_result = _context.Stock.Select(p => p).ToList();
+                var Product_result = _context.Product.Select(p => p).ToList();
+                return Ok(new { Stock_result, Product_result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("availableStock")]
+        public IActionResult AvailableStock()
+        {
+            //Product testADd = new Product();
+            //testADd.imageUrl = "12345";
+            //testADd.name = "topmuju";
+            //testADd.price = 500;
+            //_context.Product.Add(testADd);
+            //_context.SaveChangesAsync();
+            try
+            {
+                var item = _context.Stock.Where(av => av.amount > 0).ToList();
+                return Ok(item);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("Stockbyid/{id}")]
+        public IActionResult Stockbyid(string id)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(id)) return StatusCode(400);
+                //var Stock_result = _context.Stock.Where(av => av.productId == Convert.ToInt64(id)).ToList();
+                //var Product_result = _context.Product.Where(avp => avp.id == Convert.ToInt64(id)).ToList();
+                var pageObject = (from st in _context.Stock
+                                  join pr in _context.Product on st.productId equals pr.id
+                                  where st.productId == Convert.ToInt64(id) && st.amount > 0
+                                  select new { pr.id, pr.name,pr.price,pr.imageUrl, st.productId,st.amount }
+                                  ).ToList();
+                return Ok(pageObject);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Ios-api/Models/los.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ios_api.Models
+{
+
+    public class losContext : DbContext
+    {
+        public losContext(DbContextOptions<losContext> options)
+            : base(options)
+        { }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+    }
+
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new losContext(
+                serviceProvider.GetRequiredService<DbContextOptions<losContext>>()))
+            {
+                if (context.Product.Any() && context.Stock.Any()) return;
+                context.Product.AddRange(
+                    new Product
+                    {
+                        id = 1,
+                        name = "Milk",
+                        price = 100,
+                        imageUrl = "www.google.com"
+                    },
+                    new Product
+                    {
+                        id = 2,
+                        name = "Water",
+                        price = 50,
+                        imageUrl = "www.google.com"
+                    },
+                    new Product
+                    {
+                        id = 3,
+                        name = "Beer",
+                        price = 200,
+                        imageUrl = "www.google.com"
+                    },
+                    new Product
+                    {
+                        id = 4,
+                        name = "Wine",
+                        price = 300,
+                        imageUrl = "www.google.com"
+                    },
+                    new Product
+                    {
+                        id = 5,
+                        name = "Vodka",
+                        price = 400,
+                        imageUrl = "www.google.com"
+                    }
+                    );
+
+                context.Stock.AddRange(
+               new Stock
+               {
+                   id = 1,
+                   productId = 1,
+                   amount = 100,
+               },
+               new Stock
+               {
+                   id = 2,
+                   productId = 2,
+                   amount = 50,
+               },
+               new Stock
+               {
+                   id = 3,
+                   productId = 3,
+                   amount = 200,
+               },
+               new Stock
+               {
+                   id = 4,
+                   productId = 4,
+                   amount = 300,
+               },
+               new Stock
+               {
+                   id = 5,
+                   productId = 5,
+                   amount = 400,
+               }
+               );
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public class Product
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string imageUrl { get; set; }
+        public decimal? price { get; set; }
+    }
+
+    public class Stock
+    {
+        public long id { get; set; }
+        public long productId { get; set; }
+        public decimal amount { get; set; }
+    }
+}
